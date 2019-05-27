e83fe9d09e767d51832384d914e7fbc2ef75afb8
Julie Hansen <juliehansen@Julies-MBP-2.lan>
2019-05-27 16:29:30 +0200

    nav menu viser hvor på siden man befinder sig. mangler styling

--- script.js
+++ script.js
@@ -2,14 +2,38 @@ document.addEventListener("DOMContentLoaded", start);
 
 function start() {
 
-document.querySelector(".burger_menu").addEventListener("click", function () {
-    document.querySelector("nav").classList.toggle("mobile_hidden");
-    document.querySelector("header").classList.toggle("hidden");
-    document.querySelector("main").classList.toggle("hidden");
-});
+    document.querySelector(".burger_menu").addEventListener("click", function () {
+        document.querySelector("nav").classList.toggle("mobile_hidden");
+        document.querySelector("header").classList.toggle("hidden");
+        document.querySelector("main").classList.toggle("hidden");
+    });
+
+    // --------------------- nav menu ------------------
+    let mainNavLinks = document.querySelectorAll("nav ul li a");
+    let mainSections = document.querySelectorAll("main section");
+
+    let lastId;
+    let cur = [];
+
+    window.addEventListener("scroll", event => {
+        let fromTop = window.scrollY;
+
+        mainNavLinks.forEach(link => {
+            let section = document.querySelector(link.hash);
+
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                link.classList.add("current");
+            } else {
+                link.classList.remove("current");
+            }
+        });
+    });
 
 
-// ----- HVEM ER VI? -----
+    // ----- HVEM ER VI? -----
 
     destHvemErVi = document.querySelector("#hvemervi");
     async function getHvemErVi() {
@@ -44,7 +68,7 @@ document.querySelector(".burger_menu").addEventListener("click", function () {
     getHvemErVi();
 
 
-// ----- VORES TEAM -----
+    // ----- VORES TEAM -----
 
     destVoresTeam = document.querySelector("#vores_team");
     async function getVoresTeam() {
@@ -79,22 +103,22 @@ document.querySelector(".burger_menu").addEventListener("click", function () {
 
     getVoresTeam();
 
-    }
+}
 /*************** Firma og events ****************/
 
 destFirma_events = document.querySelector("#firma_events");
-    async function getFirma_events() {
-        let pagesUrl = "https://camillagejl.com/kea/2-semester/larsjon/wordpress/wp-json/wp/v2/firma_events?per_page=100";
-        let jsonData = await fetch(pagesUrl);
-        section = await jsonData.json();
-        insertFirma_events();
-    }
+async function getFirma_events() {
+    let pagesUrl = "https://camillagejl.com/kea/2-semester/larsjon/wordpress/wp-json/wp/v2/firma_events?per_page=100";
+    let jsonData = await fetch(pagesUrl);
+    section = await jsonData.json();
+    insertFirma_events();
+}
 
-    function insertFirma_events() {
-        console.log("Jeg sætter HTML ind i Firma_events");
-        section.forEach(section => {
-            let template =
-                `
+function insertFirma_events() {
+    console.log("Jeg sætter HTML ind i Firma_events");
+    section.forEach(section => {
+        let template =
+            `
            <div class="sub_section">
                         <div class="image_content">
                             <img src="${section.billede.guid}">
@@ -108,11 +132,11 @@ destFirma_events = document.querySelector("#firma_events");
                         </div>
                     </div>
 `;
-            destFirma_events.insertAdjacentHTML("beforeend", template);
-        });
-    }
+        destFirma_events.insertAdjacentHTML("beforeend", template);
+    });
+}
 
-    getFirma_events();
+getFirma_events();
 
 
 
@@ -121,14 +145,15 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
